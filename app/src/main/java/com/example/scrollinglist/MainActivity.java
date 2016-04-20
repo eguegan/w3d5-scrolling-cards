b3d934a6da8dbcb115ce6c3b72425b9aa293cb06
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-19 21:36:10 -0400

    Remove default toolbar

--- app/src/main/java/com/example/scrollinglist/MainActivity.java
+++ app/src/main/java/com/example/scrollinglist/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.scrollinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.a_main_toolbar);
+        setSupportActionBar(mToolbar);
+
+
     }
 }
