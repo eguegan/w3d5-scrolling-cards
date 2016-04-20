c8cc34da558b5ad3b77fdd80a8d123bd23e1d0fa
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-19 21:48:47 -0400

    Add custom RecyclerVIew adapter

---
+++ app/src/main/java/com/example/scrollinglist/BasicAdapter.java
@@ -0,0 +1,56 @@
+package com.example.scrollinglist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by evin on 4/19/16.
+ */
+/**
+ * Created by evin on 4/19/16.
+ */
+public class BasicAdapter extends RecyclerView.Adapter<BasicAdapter.ViewHolder> {
+
+    private ArrayList<String> mArrayList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView textViewName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textViewName = (TextView) itemView.findViewById(R.id.r_item_text);
+        }
+    }
+
+    public BasicAdapter(ArrayList<String> arrayList) {
+        this.mArrayList = arrayList;
+    }
+
+    @Override
+    public BasicAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View termView = inflater.inflate(R.layout.recycler_item, parent, false);
+
+        return new ViewHolder(termView);
+    }
+
+    @Override
+    public void onBindViewHolder(BasicAdapter.ViewHolder holder, int position) {
+        String string = mArrayList.get(position);
+
+        TextView textViewName = holder.textViewName;
+        textViewName.setText(string);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mArrayList.size();
+    }
+
+}
